89b97ae84860826305b4514d4e8c60afca2e9b8b
baijunyao <baijunyao@baijunyao.com>
2016-09-08 18:26:29 +0800

    引入自定义类

--- app/Model/AdminNav.php
+++ app/Model/AdminNav.php
@@ -3,12 +3,13 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use app\Library\Org\Data;
 class AdminNav extends Model
 {
     public function getTreeData($type='tree',$order)
     {
         $data=$this->get()->toArray();
-        p($data);
+        $test=Data::tree($data,'name','id','pid');
+        p($test);
     }
 }
