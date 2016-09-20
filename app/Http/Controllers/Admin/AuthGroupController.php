10d42cb3bfa628a1981dd9366f008b254238cead
baijunyao <baijunyao@baijunyao.com>
2016-09-20 18:22:57 +0800

    增加用户组模型

--- app/Http/Controllers/Admin/AuthGroupController.php
+++ app/Http/Controllers/Admin/AuthGroupController.php
@@ -7,81 +7,66 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Model\AuthGroup;
+
 class AuthGroupController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param  \App\Model\AuthGroup       $authGroup 用户组模型
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(AuthGroup $authGroup)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $data=$authGroup->getTreeData('tree','id');
+        $assign=[
+            'data'=>$data
+        ];
+        return View('admin.auth_rule.index', $assign);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\AuthGroup       $authGroup 用户组模型
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, AuthGroup $authGroup)
     {
-        //
+        $data=$request->all();
+        $authGroup->addData($data);
+        return redirect('admin/auth_rule/index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Model\AuthGroup       $authGroup 用户组模型
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, AuthGroup $authGroup)
     {
-        //
+        $data=$request->all();
+        $authGroup->editData($data);
+        return redirect('admin/auth_rule/index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Model\AuthGroup       $authGroup 用户组模型
+     * @param  int                       $id       权限id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(AuthGroup $authGroup, $id)
     {
-        //
+        $authGroup->deleteData($id);
+        return redirect('admin/auth_rule/index');
     }
+
+
 }
---
+++ app/Model/AuthGroup.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use DB;
+use Validator;
+use Session;
+
+class AuthGroup extends Model
+{
+    //开启如删除
+    use SoftDeletes;
+
+    /**
+     * 可以被批量赋值的属性.
+     *
+     * @var array
+     */
+    protected $fillable = ['id','title','status','rules'];
+
+    /**
+     * 自动验证
+     *
+     * @param $data 需要验证的数据
+     * @return bool 验证是否通过
+     */
+    public function validate($data)
+    {
+        $rules=[
+            'title'=>'required'
+        ];
+        $attributes=[
+            'title'=>'用户组名'
+        ];
+        $validator=Validator::make($data,$rules,[],$attributes);
+        if ($validator->fails()) {
+            $error=$validator->messages()->first();
+            Session::flash('alert-message',$error);
+            Session::flash('alert-class','alert-danger');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 添加数据
+     *
+     * @param $data 需要添加的数据
+     * @return bool 是否成功
+     */
+    public function addData($data)
+    {
+        //验证是否通过
+        if (!$this->validate($data)) {
+            return false;
+        }
+        //添加数据
+        $result=$this
+            ->create($data)
+            ->id;
+        if ($result) {
+            Session::flash('alert-message','添加成功');
+            Session::flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param $data 需要添加的数据
+     * @return bool 是否成功
+     */
+    public function editData($data)
+    {
+        //验证是否通过
+        if (!$this->validate($data)) {
+            return false;
+        }
+        $edit_data=[
+            'name'=>$data['name'],
+            'title'=>$data['title']
+        ];
+        //修改数据
+        $result=$this
+            ->where('id',$data['id'])
+            ->update($edit_data);
+        if ($result) {
+            Session::flash('alert-message','修改成功');
+            Session::flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 添加数据
+     *
+     * @param  $id  需要添加的数据
+     * @return bool 是否成功
+     */
+    public function deleteData($id)
+    {
+        //查找子权限的数量
+        $pids=$this
+            ->where('pid', $id)
+            ->count();
+        //如果有子权限；必须先删除子权限
+        if ($pids !== 0) {
+            Session::flash('alert-message','必须先删除子权限');
+            Session::flash('alert-class','alert-danger');
+            return false;
+        }
+        //删除数据
+        $result=$this
+            ->where('id',$id)
+            ->delete();
+        if ($result) {
+            Session::flash('alert-message','删除成功');
+            Session::flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+}
