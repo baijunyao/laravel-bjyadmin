5f6361677da9f2e857607c00c618350e02940679
baijunyao <baijunyao@baijunyao.com>
2016-09-29 10:48:12 +0800

    增加各种key的配置文件

---
+++ config/key.php
@@ -0,0 +1,14 @@
+<?php
+
+//各种第三方的key 和 secret
+
+return [
+    'alidayu'=>[
+        'app_key'=>env('ALIDAYU_APP_KEY', null),
+        'app_secret'=>env('ALIDAYU_APP_SECRET', null)
+    ]
+
+
+
+
+];
