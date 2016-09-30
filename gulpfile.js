912c621c3c4ea3d2edd32f4c0022b2ecb627ffb8
baijunyao <baijunyao@baijunyao.com>
2016-09-30 18:17:06 +0800

    监听多目录scss完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
-var elixir = require('laravel-elixir');
+var elixir      = require('laravel-elixir'),
+    gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    minifyCss   = require('gulp-minify-css'),
+    browserSync = require('browser-sync').create(),
+    reload      = browserSync.reload;
+
+
+new elixir.Task('bjyCss', function() {
+    return gulp.src('resources/assets/sass/**/*.scss', { base: 'resources/assets/sass'})
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'));
+}).watch('resources/assets/sass/**/*.scss');
+
 elixir(function(mix) {
-    mix.sass('app.scss').browserSync({
+    // 自动刷新
+    mix.browserSync({
         proxy: "lbjyadmin.com", // 指定代理url
         notify: false, // 刷新不弹出提示
         open: false, // 不自动打开浏览器
     });
 });
+
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,6 @@
 <html>
     <head>
         <title>Laravel</title>
-
-
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
