988a5e81133816f0dfc5876f69bd91e9c2724ec6
baijunyao <baijunyao@baijunyao.com>
2016-10-08 16:56:29 +0800

    原始方式的bootstrap模板创建完成

---
+++ resources/views/home/index/index.blade.php
@@ -0,0 +1,11 @@
+@extends('public.bootstrap')
+
+
+@section('title', '首页')
+
+
+@section('body')
+
+    首页
+
+@endsection
--- resources/views/public/bootstrap.blade.php
+++ resources/views/public/bootstrap.blade.php
@@ -5,10 +5,10 @@
     <title>@yield('title') - bjyadmin</title>
     <meta http-equiv="Cache-Control" content="no-transform" />
     <meta http-equiv="Cache-Control" content="no-siteapp" />
-    <link rel="stylesheet" href="{{asset('/statics/bootstrap-3.3.5/css/bootstrap.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('/statics/bootstrap-3.3.5/css/bootstrap-theme.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('/statics/font-awesome-4.4.0/css/font-awesome.min.css')}}" />
-    <link rel="stylesheet" href="{{elixir('/css/public/base.css')}}" />
+    <link rel="stylesheet" href="{{ asset('/statics/bootstrap-3.3.5/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/statics/bootstrap-3.3.5/css/bootstrap-theme.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/statics/font-awesome-4.4.0/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ elixir('css/public/base.css') }}" />
     @yield('css')
 </head>
 <body>
@@ -37,7 +37,7 @@
 <!-- 引入bootstrjs部分开始 -->
 <script src="{{ asset('/statics/js/jquery-1.10.2.min.js') }}"></script>
 <script src="{{ asset('/statics/bootstrap-3.3.5/js/bootstrap.min.js') }}"></script>
-<script src="{{ elixir('/js/public/base.js') }}"></script>
+<script src="{{ elixir('js/public/base.js') }}"></script>
 @yield('js')
 </body>
 </html>
