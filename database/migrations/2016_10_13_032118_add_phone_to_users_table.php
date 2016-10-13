60b0fb316cf6294cf6ca0bf674ef8aff8560679e
baijunyao <baijunyao@baijunyao.com>
2016-10-13 04:50:07 +0000

    user表增加phone和deleted_at字段

---
+++ database/migrations/2016_10_13_032118_add_phone_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPhoneToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //增加phone字段
+            $table->bigInteger('phone')
+                ->after('remember_token')
+                ->unique()
+                ->nullable()
+                ->comment('手机号');
+            //增加deleted_at字段
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
