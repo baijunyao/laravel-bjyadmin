de915a14fc4317666d73d99d31fb5edd6f202a11
baijunyao <baijunyao@baijunyao.com>
2016-10-14 15:32:42 +0800

    require jwt-auth

--- config/app.php
+++ config/app.php
@@ -178,7 +178,15 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * dingo/api
+         */
         Dingo\Api\Provider\LaravelServiceProvider::class,
+
+        /**
+         * jwt-auth
+         */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
@@ -227,7 +235,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
