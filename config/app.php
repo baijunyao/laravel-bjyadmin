92488ea2b56a733730e623f5da8bd2cdd12c6cea
baijunyao <baijunyao@baijunyao.com>
2016-10-20 11:43:20 +0800

    require iseed 并配置

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
          */
         Way\Generators\GeneratorsServiceProvider::class,
         Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
+        Orangehill\Iseed\IseedServiceProvider::class,
 
 
 
