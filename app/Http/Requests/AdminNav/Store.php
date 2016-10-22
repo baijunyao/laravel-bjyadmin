9846cb1cafb794b5fb63b63714933531eb3bf3c1
baijunyao <baijunyao@baijunyao.com>
2016-10-22 13:39:21 +0800

    增加AdminNav Request

---
+++ app/Http/Requests/AdminNav/Store.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\AdminNav;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Store extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'mca'=>'required'
+        ];
+    }
+
+    /**
+     * 定义字段名中文
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name'=>'菜单名',
+            'mca'=>'链接'
+        ];
+    }
+}
