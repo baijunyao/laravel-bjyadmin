83fa511a654e1d357b4cded6e3cafb2343170ac3
baijunyao <baijunyao@baijunyao.com>
2016-10-22 14:49:57 +0800

    创建Permissions Request

---
+++ app/Http/Requests/Permissions/Store.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Permissions;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Store extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required',
+            'display_name'=>'required'
+        ];
+    }
+
+    /**
+     * 定义字段名中文
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name'=>'权限内容',
+            'display_name'=>'权限名'
+        ];
+    }
+}
