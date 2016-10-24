b8bfd0243e07125c69bcc02ccc81867f85f2fb41
baijunyao <baijunyao@baijunyao.com>
2016-10-24 11:32:36 +0800

    增加 RoleUser model

---
+++ app/Models/RoleUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RoleUser extends Model
+{
+    /**
+     * 定义表名
+     *
+     * @var string
+     */
+    protected $table = 'role_user';
+
+    /**
+     * 不使用created_at和updated_at
+     *
+     * @var
+     */
+    public $timestamps = false;
+
+
+    /**
+     * 可以被批量赋值的属性.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'role_id'
+    ];
+}
