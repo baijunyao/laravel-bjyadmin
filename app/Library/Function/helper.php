0d75df51ff3c76aa8f3d90174612b47dea413d30
baijunyao <baijunyao@baijunyao.com>
2016-11-03 14:39:23 +0800

    完善邮件发送函数

--- app/Library/Function/helper.php
+++ app/Library/Function/helper.php
@@ -201,18 +201,18 @@ function sendSmsCode($phone, $code)
 }
 
 /**
- * 发送邮件
+ * 发送邮件函数
  *
  * @param $email            收件人邮箱  如果群发 则传入数组
- * @param $name             收件名称
+ * @param $name             收件人名称
  * @param $subject          标题
- * @param $data             邮件内容
+ * @param $data             邮件模板中用的变量 示例：['name'=>'帅白','phone'=>'110']
  * @param string $template  邮件模板
  * @return array            发送状态
  */
 function sendEmail($email, $name, $subject, $data, $template='emails.test')
 {
-    $flag = Mail::send($template, $data, function($message) use($email, $name, $subject) {
+    Mail::send($template, $data, function($message) use($email, $name, $subject) {
         //如果是数组；则群发邮件
         if (is_array($email)) {
             foreach ($email as $k => $v) {
@@ -232,10 +232,8 @@ function sendEmail($email, $name, $subject, $data, $template='emails.test')
             'status_code'=>200,
             'message'=>'邮件发送成功'
         );
-
     }
     return $data;
-
 }
 
 /**
