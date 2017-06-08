d0f16a5b2ba13375e41b6f2e9404aa6970e173aa
baijunyao <baijunyao@baijunyao.com>
2017-06-08 17:44:22 +0800

    增加对字符串执行指定次数替换的函数

--- app/Library/Function/helper.php
+++ app/Library/Function/helper.php
@@ -498,3 +498,25 @@ if (! function_exists('flashMessage')){
         session()->flash('alert-class', $className);
     }
 }
+
+if (! function_exists('strReplaceLimit')) {
+    /**
+     * 对字符串执行指定次数替换
+     * @param  Mixed $search   查找目标值
+     * @param  Mixed $replace  替换值
+     * @param  Mixed $subject  执行替换的字符串／数组
+     * @param  Int   $limit    允许替换的次数，默认为 - 1，不限次数
+     * @return Mixed
+     */
+    function strReplaceLimit($search, $replace, $subject, $limit=-1)
+    {
+        if(is_array($search)){
+            foreach($search as $k=>$v){
+                $search[$k] = '`'. preg_quote($search[$k], '`'). '`';
+            }
+        }else{
+            $search = '`'. preg_quote($search, '`'). '`';
+        }
+        return preg_replace($search, $replace, $subject, $limit);
+    }
+}
